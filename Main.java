7bd65017a8ebdcdd76a70dfe6902db2debbb000e
JakeNichols0 <161376788+JakeNichols0@users.noreply.github.com>
2025-05-12 19:11:44 +0000

    W sesh 2

--- Main.java
+++ Main.java
@@ -6,20 +6,29 @@ import java.util.Arrays;
 
 public class Main {
   public static void main(String[] args) {
-    System.out.println("This is a simulation of a slot machine using a computer.");
-    System.out.println("Each time you 'pull' I will ask you if you wish to play again.");
-    System.out.println("Just answer with a 'y' for yes or a 'n' for no.");
-    System.out.println("The more you put in; the more you can get out. \n");
 
     SlotMachine slots = new SlotMachine();
     Scanner sc = new Scanner(System.in);
-    boolean again = true;
-    while(again) {
-      slots.pull();
-      System.out.print(slots + " - Again? ");
-      String s = sc.next();
+    //Create the person
+    System.out.print("What's your name? ");
+    Person p = new Person(sc.nextLine());
+    System.out.println("You have $" + p.getMoney());
+    //Ask for bet
+    System.out.print("How much money? $");
+    int bet = sc.nextInt();
+    sc.nextLine();
+    while(bet > 0) {
+      //Play the game
+      slots.pull(bet);
+      System.out.println(slots);
+      System.out.print("You have $" + p.getMoney() + " How you bettin' bbg? $");
+      bet = sc.nextInt();
       sc.nextLine();
-      again = s.equals("y");
+      while (bet > p.getMoney()) {
+        System.out.print("You can't bet more money than you have. How you bettin' bbg? $");
+        bet = sc.nextInt();
+        sc.nextLine();
+      }
     }
   }
   
--- SlotMachine.java
+++ SlotMachine.java
@@ -19,9 +19,30 @@ public class SlotMachine {
 
 	public int calc() { //If you're just tuning in, calc is slang for calculator
 		//two match 1/2 of in; 3 match 2x in
+		int winnings = 0;
+		int[] matches = new int[6]; //Ordered the same as options
+		for(String s : slots) {
+			for(int i=0; i < options.length; i++) {
+				if(s.equals(options[i])) {
+					matches[i]++;
+					break;
+				}
+			}
+		}
+		for(int j=0; j < matches.length; j++) {
+			winnings += ((0.2 * factorial(matches[j])) - 0.2) * moneyIn;
+		}
+		return winnings;
+	}
+
+	private int factorial(int n) {
+		if(n == 0) {
+			return 1;
+		}
+		return factorial(n-1) * n;
 	}
 
 	public String toString() {
-		return slots[0] + " | " + slots[1] + " | " + slots[2];
+		return slots[0] + " | " + slots[1] + " | " + slots[2] + " - You won $" + calc();
 	}
 }
